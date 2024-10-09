481b77422b1259312f18fa530ce29f0639552768
grmang0 <grmangox@gmail.com>
2024-10-09 14:42:57 +0700

    Add initial code for image handler

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import ImageHandler from './components/ImageHandler';
+
+function App() {
+    return (
+        <div className="container">
+            <ImageHandler />
+        </div>
+    );
+}
+
+export default App;
